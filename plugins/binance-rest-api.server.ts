8fb92aa9c6a87e5fae4330a910db4f74e105c931
Wirbelwind <komendantov2580@gmail.com>
2025-03-13 20:08:17 +0200

    format

--- plugins/binance-rest-api.server.ts
+++ plugins/binance-rest-api.server.ts
@@ -1,16 +1,16 @@
-import { Spot } from "@binance/connector-typescript";
+import { Spot } from '@binance/connector-typescript'
 
 export default defineNuxtPlugin({
   parallel: true,
   async setup() {
-    const config = useRuntimeConfig();
+    const config = useRuntimeConfig()
 
-    const restClient = new Spot(config.public.apiKey, config.public.secretKey);
+    const restClient = new Spot(config.public.apiKey, config.public.secretKey)
 
     return {
       provide: {
         restClient,
       },
-    };
+    }
   },
-});
+})
