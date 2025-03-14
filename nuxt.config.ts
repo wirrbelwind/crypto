e3edace36f534897e8d0ef67e74be41f378fdebe
Wirbelwind <komendantov2580@gmail.com>
2025-03-14 19:38:21 +0200

    install pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['nuxt-quasar-ui', '@nuxt/eslint'],
+  modules: ['nuxt-quasar-ui', '@nuxt/eslint', '@pinia/nuxt'],
 
   runtimeConfig: {
     public: {
