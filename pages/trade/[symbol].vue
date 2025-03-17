4ab3b92bc560ba93215f174c9912fb41eca483a5
Wirbelwind <komendantov2580@gmail.com>
2025-03-17 21:49:45 +0200

    use symbol store in trade page

--- pages/trade/[symbol].vue
+++ pages/trade/[symbol].vue
@@ -1,26 +1,36 @@
 <script setup lang="ts">
 import { BinanceSymbolWSS } from '~/utils/binance-ws-stream/BinanceSymbolWSS'
+import { useSymbolStore } from '../../stores/symbol'
 
 const route = useRoute()
-const symbol = Array.isArray(route.params.symbol) ? route.params.symbol[0] : route.params.symbol 
+const symbolUrlParam = Array.isArray(route.params.symbol)
+  ? route.params.symbol[0]
+  : route.params.symbol
 
 let wss: BinanceSymbolWSS
 
-const aggregateTradeStore = useAggregateTrade()
+const symbolStore = useSymbolStore()
 
 onMounted(async () => {
-  aggregateTradeStore.$patch({
-    symbol: symbol,
+  symbolStore.$patch({
+    symbol: symbolUrlParam,
   })
 
-  wss = new BinanceSymbolWSS(symbol, {
+  wss = new BinanceSymbolWSS(symbolUrlParam, {
     baseUrl: 'wss://stream.binance.com:9443',
     messageHandlers: {
       aggTrade: [
         (message) => {
-          aggregateTradeStore.$patch({
-            // @ts-expect-error qwertyyuuuu
-            price: message.p,
+          symbolStore.$patch({
+            // @ts-expect-error qweqeqeqeqeq
+            _price: message.p,
+          })
+        },
+      ],
+      kline: [
+        (message) => {
+          symbolStore.$patch({
+            kline: message,
           })
         },
       ],
@@ -28,14 +38,18 @@ onMounted(async () => {
   })
   await wss.connect()
   wss.aggTrade()
+  wss.kline(1, 'M')
 })
 
 onUnmounted(() => {
   wss?.disconnect()
 })
-
 </script>
 
 <template>
-  <p>Price {{ aggregateTradeStore.symbol }}: {{ aggregateTradeStore.formattedPrice }}</p>
+  <div>
+    <p>Price {{ symbolStore.symbol }}: {{ symbolStore.price }}</p>
+
+    <p>candles: {{ symbolStore.kline }}</p>
+  </div>
 </template>
